d232ad20e57b9e48272db36a6a44c0dd869d66de
subin932 <asubin1345@naver.com>
2025-05-26 10:11:47 +0900

    Add files via upload

---
+++ js/chart_style.js
@@ -0,0 +1,14 @@
+$ (function(){	
+	$('.chart3').easyPieChart({
+		barColor: '#588953',
+		trackColor: '#C0CE9A',
+		scaleColor: '#C0CE9A',
+		lineCap: 'square',
+		lineWidth: 25,
+		size: 200,
+		animate: 2000,
+		onStart: $.noop,
+		onStop: $.noop
+	});
+	
+ }) ; 
